6b378fe132b8fe02172eda5e76298b07a2036bf2
Zakariya Khan <zakariyakhan1@gmail.com>
2024-02-26 22:30:12 +0530

    Receipt Changes

--- src/app/models/receipt.model.ts
+++ src/app/models/receipt.model.ts
@@ -1,4 +1,4 @@
-export interface ProductModel{
+export interface ProductModel {
   _id: Number
   name: String,
   receiptDate: String,
@@ -7,4 +7,31 @@ export interface ProductModel{
   sgst: Number,
   grandTotal: Number,
   products: String
+}
+
+export class InvoiceModel {
+  customerName!: string;
+  productName!: string;
+  quantity!: number;
+  receiptDate!: string;
+  rate!: number;
+  taxableAmount!: number;
+  gst!: number;
+  cgst!: number;
+  sgst!: number;
+  igst!: number;
+  total!: number
+}
+
+export class ReceiptModel {
+  id!: string;
+  name!: string;
+  mobileNumber!: number;
+  receiptDate!: string;
+  taxableAmount!: number;
+  cgst!: number;
+  sgst!: number;
+  grandTotal!: number;
+  products!: ProductModel[];
+  gst!: number
 }
--- src/app/pages/add-receipt/add-receipt.component.ts
+++ src/app/pages/add-receipt/add-receipt.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 import { HttpErrorResponse } from '@angular/common/http';
 declare var jQuery: any;
 import { Router } from '@angular/router';
@@ -8,6 +8,7 @@ import { AppConstants } from '../../shared/app-contants.service';
 import { AppStrings } from '../../shared/app-strings.service';
 import { AppUtilityService } from '../../core/services/app-utility.service';
 import { Subject, takeUntil } from 'rxjs';
+import { InvoiceModel, ReceiptModel } from '../../models/receipt.model';
 
 @Component({
   selector: 'app-add-receipt',
@@ -17,52 +18,25 @@ import { Subject, takeUntil } from 'rxjs';
 export class AddReceiptComponent implements OnInit, OnDestroy {
   // frmReceipt!: FormGroup;
   frmReceipt!: any;
-  _id = '';
-  receipt = {
-    _id: '',
-    name: '',
-    mobileNumber: '',
-    receiptDate: '',
-    taxableAmount: 0,
-    cgst: 0,
-    sgst: 0,
-    grandTotal: 0,
-    products: [],
-    gst: ''
-  };
+  id = '';
+  receipt: ReceiptModel = new ReceiptModel();
   subscription = new Subject();
   grandTotal = 0;
-  receiptDate!: any;
-  mobileNumber!: any;
-  today!: any;
-  receiptData = JSON.parse(localStorage.getItem('receipt') || "{}");
-  custName!: any;
-  rcDate!: any;
-  taxableAmountSum = 0;
-  sgstSum = 0;
-  cgstSum = 0;
-  gst!: any;
+  receiptDate!: string;
+  mobileNumber!: number;
+  custName!: string;
+  rcDate!: string;
+  taxableAmountSum: number = 0;
+  sgstSum: number = 0;
+  cgstSum: number = 0;
   p = 1;
   invoiceArray: any = [];
-  invoice = {
-    customerName: '',
-    productName: '',
-    quantity: 0,
-    receiptDate: '',
-    rate: 0,
-    taxableAmount: 0,
-    gst: 0,
-    cgst: 0,
-    sgst: 0,
-    igst: 0,
-    total: 0
-  }
+  invoice: InvoiceModel = new InvoiceModel()
   submitted = false;
-  textSearch = '';
-  credits!: any[];
+  textSearch: string = '';
   products!: any[];
 
-  selectedFile = null;
+  selectedFile: any = null;
   RECEIPT_INVOICE_COLUMNS: string[] = [];
   warningText: string = '';
   appStrings: any;
@@ -77,31 +51,23 @@ export class AddReceiptComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.frmReceipt = this.formBuilder.group({
-      _id: [""],
+      id: [""],
       customerName: ["", Validators.required],
-      receiptDate: ["", Validators.required],
-      mobileNumber: [""],
-      gst: [0],
-      cgst: [0],
-      sgst: [0],
-      igst: [0],
-      tax: [0],
-      productName: [null],
-      quantity: [""],
-      rate: [""]
+      receiptDate: [(new Date()).toISOString().substring(0, 10), Validators.required],
+      mobileNumber: [, Validators.maxLength(10)],
+      gst: [],
+      cgst: [],
+      sgst: [],
+      igst: [],
+      tax: [],
+      productName: [null, Validators.required],
+      quantity: [, Validators.required],
+      rate: [, Validators.required]
     });
+
     this.appStrings = this.appStringsService.appStrings;
     this.RECEIPT_INVOICE_COLUMNS = this.appConstants.RECEIPT_INVOICE_COLUMNS;
     this.getProducts();
-    this.getDate();
-    this.editReceipt();
-  }
-
-
-  //Set Current Day Date to the receipt date
-  getDate() {
-    this.today = new Date();
-    this.frmReceipt?.get('receiptDate')?.setValue(this.today);
   }
 
   //GST SUM calculation Start
@@ -115,24 +81,8 @@ export class AddReceiptComponent implements OnInit, OnDestroy {
 
   //GST calculation Start
   calc() {
-    if (this.frmReceipt.get('productName').value == '' || this.frmReceipt.get('productName').value == undefined || this.frmReceipt.get('productName').value <= 0) {
-      // this.toaster.warningToastr('Please select product!', 'Invalid!', {showCloseButton: true});
-      return;
-    }
-    if (this.frmReceipt.get('quantity').value == '' || this.frmReceipt.get('quantity').value == undefined || this.frmReceipt.get('quantity').value <= 0) {
-      // this.toaster.warningToastr('Quantity should be more than zero!', 'Invalid!', {showCloseButton: true});
-      return;
-    }
-    if (this.frmReceipt.get('rate').value == '' || this.frmReceipt.get('rate').value == undefined || this.frmReceipt.get('rate').value <= 0) {
-      // this.toaster.warningToastr('Rate should be more than zero!', 'Invalid!', {showCloseButton: true});
-      return;
-    }
-    this.frmReceipt.get('receiptDate').setValue(this.today);
-    this.receiptDate = this.frmReceipt.get('receiptDate').value;
-    this.mobileNumber = this.frmReceipt.get('mobileNumber').value;
-    this.invoice.customerName = this.frmReceipt.get('customerName').value;
-    this.custName = this.frmReceipt.get('customerName').value;
-    this.rcDate = this.frmReceipt.get('receiptDate').value;
+    console.log(this.frmReceipt?.value);
+
     this.invoice.quantity = this.frmReceipt.get('quantity').value;
     this.invoice.receiptDate = this.frmReceipt.get('receiptDate').value;
     this.invoice.rate = this.frmReceipt.get('rate').value;
@@ -141,7 +91,6 @@ export class AddReceiptComponent implements OnInit, OnDestroy {
     this.invoice.taxableAmount = this.invoice.rate * this.invoice.quantity;
     this.taxableAmountSum += this.invoice.taxableAmount;
     this.invoice.gst = this.frmReceipt.get('gst').value;
-    this.gst = this.frmReceipt.get('gst').value;
     if (this.invoice.gst == undefined || this.invoice.gst == null) {
       this.invoice.cgst = 0;
       this.invoice.sgst = 0;
@@ -174,37 +123,32 @@ export class AddReceiptComponent implements OnInit, OnDestroy {
       igst: 0,
       total: 0
     }
-    this.frmReceipt.reset();
     this.submitted = false;
-    this.frmReceipt.get('receiptDate').setValue(this.today);
-    this.frmReceipt.get('customerName').setValue(this.custName);
-    this.frmReceipt.get('mobileNumber').setValue(this.mobileNumber);
-    this.frmReceipt.get('gst').setValue(this.invoice.gst);
+    this.frmReceipt.get('quantity').setValue('');
+    this.frmReceipt.get('rate').setValue('');
+    this.frmReceipt.get('productName').setValue(null);
   }
 
+  /**
+   * routing back to receipt listing 
+   */
   backToReceipt() {
     this.router.navigate(['default/receipt']);
   }
 
-  //Edit Receipt
-  editReceipt() {
-    if (this.receiptData != null) {
-      this._id = this.receiptData._id
-      this.frmReceipt.get('customerName').setValue(this.receiptData.name);
-      this.frmReceipt.get('receiptDate').setValue(this.receiptData.receiptDate);
-      this.frmReceipt.get('gst').setValue(parseInt(this.receiptData.gst));
-      this.gst = parseInt(this.receiptData.gst)
-      this.frmReceipt.get('mobileNumber').setValue(this.receiptData.mobileNumber);
-      this.mobileNumber = this.receiptData.mobileNumber;
-      this.taxableAmountSum = parseInt(this.receiptData.taxableAmount);
-      this.grandTotal = parseInt(this.receiptData.grandTotal);
-      this.cgstSum = parseInt(this.receiptData.cgst);
-      this.sgstSum = parseInt(this.receiptData.cgst);
-      this.invoiceArray = this.receiptData.products;
-      this.custName = this.receiptData.name;
-      this.rcDate = this.receiptData.receiptDate;
+  /**
+   * 
+   */
+  setProductPrice() {
+    console.log(this.products)
+    const product = this.products?.find(item => item.name == this.frmReceipt.get('productName').value);
+    if (product) {
+      this.frmReceipt.get('rate').setValue(product?.price);
+    } else {
+      this.frmReceipt.get('rate').setValue('');
     }
-  };
+    console.log(this.frmReceipt.get('productName').value)
+  }
 
   //POST package
   onSubmit() {
@@ -216,105 +160,59 @@ export class AddReceiptComponent implements OnInit, OnDestroy {
     this.receipt.grandTotal = this.grandTotal;
     this.receipt.products = this.invoiceArray;
     this.receipt.mobileNumber = this.mobileNumber;
-    this.receipt.gst = this.gst;
-    this.receipt._id = this._id;
+    this.receipt.gst = this.frmReceipt?.get('gst')?.value;
+    this.receipt.id = this.id;
     this.submitted = true;
     if (this.frmReceipt.invalid) {
       // this.toaster.warningToastr('Please enter mendatory fields.', 'Invalid!', {showCloseButton: true});
       return;
     }
-    if (this.receipt._id == '') {
-      // this.receipt.receiptDate = moment(this.frmReceipt.value.date).format('DD-MM-YYYY') ;
-      console.log('this.receipt');
-      console.log(this.receipt);
-      return
-      this.commonService.saveReceipt(this.receipt).subscribe((response: any) => {
-        if (response.status) {
-          // this.toaster.successToastr('Receipt saved successfully. ', 'Success!',{showCloseButton: true});
-          this.receipt = {
-            _id: '',
-            name: '',
-            mobileNumber: '',
-            receiptDate: '',
-            taxableAmount: 0,
-            cgst: 0,
-            sgst: 0,
-            grandTotal: 0,
-            products: [],
-            gst: ''
-          };
-          this.taxableAmountSum = 0;
-          this.cgstSum = 0;
-          this.sgstSum = 0;
-          this.grandTotal = 0
-          this._id = '';
-          this.invoiceArray = [];
-          this.frmReceipt.reset();
-          this.submitted = false;
-          // this.getCredits();
-          // jQuery('#addCredit').modal('hide');
-          //  this.getpackage();
-        } else {
-          // this.toaster.errorToastr('Error while saving receipt.', 'Oops!',{showCloseButton: true});
+    // this.receipt.receiptDate = moment(this.frmReceipt.value.date).format('DD-MM-YYYY') ;
+    console.log('this.receipt');
+    console.log(this.receipt);
+    return
+    this.commonService.saveReceipt(this.receipt).subscribe((response: any) => {
+      if (response.status) {
+        // this.toaster.successToastr('Receipt saved successfully. ', 'Success!',{showCloseButton: true});
+        this.receipt = {
+          id: '',
+          name: '',
+          mobileNumber: 0,
+          receiptDate: '',
+          taxableAmount: 0,
+          cgst: 0,
+          sgst: 0,
+          grandTotal: 0,
+          products: [],
+          gst: 0
+        };
+        this.taxableAmountSum = 0;
+        this.cgstSum = 0;
+        this.sgstSum = 0;
+        this.grandTotal = 0
+        this.id = '';
+        this.invoiceArray = [];
+        this.frmReceipt.reset();
+        this.submitted = false;
+      } else {
 
-        }
-      }, (error: HttpErrorResponse) => {
-        // this.toaster.errorToastr('Error while saving credit.', 'Oops!',{showCloseButton: true});
-        return;
-      });
-    }
-    else {
-      // this.receipt.receiptDate = moment(this.frmReceipt.value.date).format('DD-MM-YYYY') ;
-      this.commonService.updateReceipt(this.receipt._id, this.receipt).subscribe((response: any) => {
-        if (response.status) {
-          // this.toaster.successToastr('Invoice updated successfully. ', 'Success!',{showCloseButton: true});
-          // this.getCredits();
-          this.receipt = {
-            _id: '',
-            name: '',
-            mobileNumber: '',
-            receiptDate: '',
-            taxableAmount: 0,
-            cgst: 0,
-            sgst: 0,
-            grandTotal: 0,
-            products: [],
-            gst: ''
-          };
-          this._id = '';
-          this.taxableAmountSum = 0;
-          this.cgstSum = 0;
-          this.sgstSum = 0;
-          this.grandTotal = 0
-          this.frmReceipt.reset();
-          this.submitted = false
-          this.invoiceArray = [];
-          this.router.navigate(['default/receipt']);
-        } else {
-          // this.toaster.errorToastr('Error while saving receipt.', 'Oops!',{showCloseButton: true});
-        }
-      }, (error: HttpErrorResponse) => {
-        // this.toaster.errorToastr('Error while saving receipt.', 'Oops!',{showCloseButton: true});
-        return;
-      });
-    }
+      }
+    }, (error: HttpErrorResponse) => {
+      return;
+    });
   }
-  get f() { return this.frmReceipt.controls; }
+
 
   /**
   * get products data
   */
   getProducts(): void {
-    this.warningText = 'Loading Data...';
+    this.commonService.$loaderSubject?.next({ showLoader: true });
     this.commonService.getProducts().pipe(takeUntil(this.subscription)).subscribe((response: any) => {
       this.products = response;
-      console.log('this.products');
-      console.log(this.products);
-      this.warningText = 'No Data Found!';
+      this.commonService.$loaderSubject?.next({ showLoader: false });
     }, (error: HttpErrorResponse) => {
-      console.log('error text')
-      console.log(error)
-      this.warningText = 'No Data Found!';
+      this.commonService.$loaderSubject?.next({ showLoader: false });
       this.commonService.$alertSubject?.next({
         type: 'danger',
         showAlert: true,
@@ -322,7 +220,6 @@ export class AddReceiptComponent implements OnInit, OnDestroy {
       });
     });
   }
-
 
   //Edit invoiceRow
   editinvoiceRow(index: any, data: any) {
@@ -344,42 +241,11 @@ export class AddReceiptComponent implements OnInit, OnDestroy {
 
   }
 
-  //Delete package
-  deleteCredit(id: any) {
-    // Swal.fire({
-    //   title: 'Are you sure?',
-    //   text: 'You will not be able to recover this record!',
-    //   icon: 'warning',
-    //   showCancelButton: true,
-    //   cancelButtonColor: '#d33',
-    //   confirmButtonText: 'Delete',
-    //   cancelButtonText: 'Cancel'
-    // })
-    // .then((result) => {
-    //   if (result.value) {
-
-    //     this.commonService.deleteProduct(id).subscribe((response : any)=>{
-    //       if (response.status) {
-    //         // this.toaster.successToastr('Deleted successfully. ', 'Success!',{showCloseButton: true});
-    //         // this.getCredits();
-    //       }else {
-    //         // this.toaster.errorToastr('Error while deleting credit.', 'Oops!',{showCloseButton: true});
-    //       }
-    //     }, (error: HttpErrorResponse) => {
-    //       // this.toaster.errorToastr('Error while deleting credit.', 'Oops!',{showCloseButton: true});
-    //       return;
-    //     });
-    //   }});
-  }
-
-  //Package Date Conversion
-  dateConverter(date: any) {
-    var dateArray = date.split('-');
-    var dateStr = dateArray[1] + '/' + dateArray[0] + '/' + dateArray[2];
-    var newDate = new Date(dateStr);
-    return newDate;
-  }
-
+  /**
+   * 
+   * @param index 
+   * @param data 
+   */
   removeRow(index: any, data: any) {
     this.invoiceArray.splice(index, 1);
     this.grandTotal = this.grandTotal - data.total;
@@ -388,7 +254,14 @@ export class AddReceiptComponent implements OnInit, OnDestroy {
     this.sgstSum = this.sgstSum - data.sgst;
   }
 
-  // clear form value
+  /**
+   * 
+   */
+  get f() { return this.frmReceipt.controls; }
+
+  /**
+   * 
+   */
   clearForm() {
     this.frmReceipt.reset();
     this.submitted = false;
